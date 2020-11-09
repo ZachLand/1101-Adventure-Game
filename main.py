52e578c65bacb0364916a4ad9ede8998c83e3817
ZachLand <tyler.z.landgraf@gmail.com>
2020-11-09 01:36:17 +0000

    Added some comments for steps

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 print("This is a placeholder for our Adventure Game group project.")
 # Adventure Game Group Project for 1101
+#AlanLopez
+# Intro
+  # Get player name from keyboard.
+  # Display rules, player controls (UI) and objective.
+# Input from user for which room they'd like to enter.
