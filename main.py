3b4d509bff39b5658659d30a6fafb50f1483c171
ZachLand <tyler.z.landgraf@gmail.com>
2020-11-10 02:30:32 +0000

    More Pseudocode

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 print("This is a placeholder for our Adventure Game group project.")
 # Adventure Game Group Project for 1101
-#AlanLopez
+#AlanLopez, Henry Olvera, Tyler Landgraf
 # Intro
   # Get player name from keyboard.
   # Display rules, player controls (UI) and objective.
+# Display all the current  room options to the available player
 # Input from user for which room they'd like to enter.
+  # Display all available options to the player once they pick a room, as well as give them a description of what's going on in the room
+  # Display the player's options depending on what room they are currently in
+  # Once the player has finished all objectives in the current room, allow them to look for items
+  # Display the option of continuing to the next room
+# Repeat lines 8 through 11 until the player has beaten all rooms
+# Once the player has defeated 8 rooms
+  # Have the final room be a boss room
+  # Display what is going in the room to the player
+  # Display all available options/actions to the player
+  # Each option/action will have its own consequence
+  # Depending on the player's actions, he will either be succesful or fail
+
